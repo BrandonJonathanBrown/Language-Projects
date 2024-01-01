c8d9a790abbf03231fd6cc31dd23602db67ae35e
Brandon Jonathan Brown <61449380+BrandonJonathanBrown@users.noreply.github.com>
2024-01-01 07:25:12 -0600

    Update bootload.asm

--- bootload.asm
+++ bootload.asm
@@ -1,14 +1,14 @@
-ORG   0x7C00
+ORG   0x7C00 ;Author Brandon Jonathan Brown
 BITS  16
 
 ; IN (dl)
-mov dh, 0x00     ; DL is bootdrive
+mov dh, 0x00     
 mov cx, 0x0002
 mov bx, 0x1000
 mov es, bx
 xor bx, bx
-mov ax, 0x0201   ; BIOS.ReadSector
-int 0x13         ; -> AH CF
+mov ax, 0x0201   
+int 0x13         
 jc  ERR
 jmp 0x1000:0x0000
 
