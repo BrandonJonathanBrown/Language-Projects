c7d7247a88080906c09d437dc76d819389f8aead
Brandon Jonathan Brown <61449380+BrandonJonathanBrown@users.noreply.github.com>
2024-03-29 11:38:57 -0500

    Update FileEncryption.py

--- FileEncryption.py
+++ FileEncryption.py
@@ -1,49 +1,49 @@
 import pyaes, pbkdf2, binascii, os, secrets
-class File:
 
-    global filename
+#Author Brandon Jonathan Brown
+
+class FileEncryption:
 
     def __init__(self):
         print("[*] Starting Application!")
-
-        self.File_Text()
-        self.Key()
-        self.Encrypt()
-        self.Decrypt()
-
-
-    def File_Text(self):
         self.filename = input("Please Enter A Name For The File: ")
-        self.txt = open(self.filename, "w+")
         self.text = input("Please Enter Text To Write: ")
-        self.txt.write(self.text)
+        self.password = "34*7^%##@!"  # Consider using a more secure approach
+        self.file_text()
+        self.key_salt_init()
+        self.encrypt()
+        self.decrypt()
 
-    def Key(self):
-        self.password = "34*7^%##@!"
+    def file_text(self):
+        """Writes the input text to the specified file."""
+        with open(self.filename, "w") as txt:
+            txt.write(self.text)
+
+    def key_salt_init(self):
+        """Initializes key and salt for encryption."""
         self.passwordSalt = os.urandom(16)
         self.key = pbkdf2.PBKDF2(self.password, self.passwordSalt).read(32)
         print('AES encryption key:', binascii.hexlify(self.key))
+        # Initialize IV
+        self.iv = secrets.randbelow(1 << 128)  # Proper size for AES CTR mode
 
+    def encrypt(self):
+        """Encrypts the content of the file."""
+        aes = pyaes.AESModeOfOperationCTR(self.key, pyaes.Counter(self.iv))
+        with open(self.filename, "r") as txt:
+            content = txt.read()
+        ciphertext = aes.encrypt(content)
+        with open(self.filename, "wb") as txt:
+            txt.write(ciphertext)
+        print('Encrypted:', binascii.hexlify(ciphertext))
 
-    def Encrypt(self):
-        self.iv = secrets.randbits(256)
-        self.txt = open(self.filename, "r")
-        self.aes = pyaes.AESModeOfOperationCTR(self.key, pyaes.Counter(self.iv))
-        self.ciphertext = ""
-        if self.txt.mode == "r":
-            self.content = self.txt.read()
-            self.ciphertext = self.aes.encrypt(self.content)
-        self.txt = open(self.filename, "wb")
-        self.txt.write(self.ciphertext)
-        print('Encrypted:', binascii.hexlify(self.ciphertext))
-
-    def Decrypt(self):
-        self.aes = pyaes.AESModeOfOperationCTR(self.key, pyaes.Counter(self.iv))
-        self.txt = open(self.filename, "rb")
-        self.decrypted = self.aes.decrypt(self.txt.read())
-        print('Decrypted:', self.decrypted)
-
+    def decrypt(self):
+        """Decrypts the content of the file."""
+        aes = pyaes.AESModeOfOperationCTR(self.key, pyaes.Counter(self.iv))
+        with open(self.filename, "rb") as txt:
+            decrypted = aes.decrypt(txt.read())
+        print('Decrypted:', decrypted.decode("utf-8"))  # Assuming the original text is UTF-8
 
 if __name__ == "__main__":
-    Obj = File()
+    fileEncryption = FileEncryption()
 
