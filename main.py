d0e1249b5fb424c31ae9f1519cd90bf989dab169
Brandon Jonathan Brown <61449380+BrandonJonathanBrown@users.noreply.github.com>
2024-03-29 11:27:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
-#Author Brandon Jonathan Brown
+# Author: Brandon Jonathan Brown
 
 class QuickSort:
-
     def __init__(self):
         print("[*] Starting Application!")
 
-
     def quicksort(self, arr):
-
+        """Sorts an array using the quicksort algorithm."""
         if len(arr) <= 1:
             return arr
         else:
             pivot = arr[0]
-            less = [x for x in arr[1:] if x <= pivot]
-            greater = [x for x in arr[1:] if x >= pivot]
+            less = [x for x in arr[1:] if x < pivot]  # Elements less than pivot
+            equal = [x for x in arr if x == pivot]    # Elements equal to pivot (including pivot itself)
+            greater = [x for x in arr[1:] if x > pivot]  # Elements greater than pivot
 
-            return self.quicksort(less) + [pivot] + self.quicksort(greater)
-
+            return self.quicksort(less) + equal + self.quicksort(greater)
 
 if __name__ == "__main__":
-
-    Obj = QuickSort()
-
-    l = [8,11,4,7,3,2,9,45,33]
-
-    print(Obj.quicksort(l))
+    qs = QuickSort()
+    lst = [8, 11, 4, 7, 3, 2, 9, 45, 33, 2]
+    print(qs.quicksort(lst))
