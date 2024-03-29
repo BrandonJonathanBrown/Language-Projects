0cc7e0e88d67dbbdc8c04b757dfbb4c714884df0
Brandon Jonathan Brown <61449380+BrandonJonathanBrown@users.noreply.github.com>
2024-03-29 12:49:39 -0500

    Update SinglyLinkedList.cpp

--- SinglyLinkedList.cpp
+++ SinglyLinkedList.cpp
@@ -1,106 +1,92 @@
-/**
-@Author Brandon Jonathan Brown
-@About Simple Singly linked list data structure c++
-*/
-#include <iostream>  
+#include <iostream>
+#include <memory>
 
-template <class T> class Node
-{
+// Author Brandon Jonathan Brown
 
+template <class T>
+class Node {
 public:
-    
     T data;
-    Node<T>* next;
+    std::unique_ptr<Node<T>> next;
 
+    Node() : data(0), next(nullptr) {}
+    Node(T data) : data(data), next(nullptr) {}
 
-    Node()
-    {
-        data = 0;
-        next = nullptr;
-    }
+    // Disable copy operations
+    Node(const Node&) = delete;
+    Node& operator=(const Node&) = delete;
 
-    Node(T data)
-    {
-        this->data = data;
-    }
-    
- 
+    // Enable move operations
+    Node(Node&&) noexcept = default;
+    Node& operator=(Node&&) noexcept = default;
+
+    ~Node() = default;
 };
 
-
-template <class T> class SinglyLinkList
-{
-
+template <class T>
+class SinglyLinkList {
 public:
+    std::unique_ptr<Node<T>> head;
 
-    Node<T>* head;
+    SinglyLinkList() : head(nullptr) {}
 
-    SinglyLinkList() :head(nullptr)
-    {
-    }
+    // Disable copy operations
+    SinglyLinkList(const SinglyLinkList&) = delete;
+    SinglyLinkList& operator=(const SinglyLinkList&) = delete;
 
+    // Enable move operations
+    SinglyLinkList(SinglyLinkList&&) noexcept = default;
+    SinglyLinkList& operator=(SinglyLinkList&&) noexcept = default;
 
-    void Insert(T data)
-    {
-        Node<T>* newNode = new Node<T>(data);
-        
-        if (head == nullptr)
-        {
-           head = newNode;
-        }
-        else
-        {
-            newNode->next = head;
-            head = newNode;
-        }
-
-    }
-
-    void Delete()
-    {
-        if (head == nullptr)
-        {
-            std::cout << "List is empty!" << std::endl;
-        }
-        else
-        {
-            Node<T>* temp = head;
-            head = head->next;
-            delete temp;
-
+    ~SinglyLinkList() {
+        while (head != nullptr) {
+            head = std::move(head->next);
         }
     }
 
-    void ListTraversal()
-    {
-        Node<T>* current = head;
-
-        while (current != nullptr)
-        {
-            std::cout << current->data << ",";
-            current = current->next;
+    void Insert(T data) {
+        auto newNode = std::make_unique<Node<T>>(data);
+        if (!head) {
+            head = std::move(newNode);
+        } else {
+            newNode->next = std::move(head);
+            head = std::move(newNode);
         }
     }
-      
 
+    void Delete() {
+        if (!head) {
+            std::cout << "List is empty!\n";
+        } else {
+            head = std::move(head->next);
+        }
+    }
 
+    void ListTraversal() {
+        if (!head) {
+            std::cout << "List is empty!\n";
+            return;
+        }
+        Node<T>* current = head.get();
+        while (current != nullptr) {
+            std::cout << current->data;
+            current = current->next.get();
+            if (current) std::cout << ", ";
+        }
+        std::cout << std::endl;
+    }
 };
 
-
-
-
-int main(int argc, char** argv)
-{
-
+int main() {
     SinglyLinkList<int> list;
-    
+
     list.Insert(6);
     list.Insert(7);
     list.Insert(12);
 
     list.Delete();
-
     list.ListTraversal();
 
-    return -1;
-};
+    return 0;
+}
+
