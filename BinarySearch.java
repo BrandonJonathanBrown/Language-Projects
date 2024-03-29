97f49062d5d312270c4cdaedc97fa757aa7231bb
Brandon Jonathan Brown <61449380+BrandonJonathanBrown@users.noreply.github.com>
2024-03-29 12:45:10 -0500

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,52 +1,57 @@
-import java.lang.Runnable;
+import java.util.Arrays;
 import java.util.Scanner;
 
-/**
- * @Author Brandon Jonathan Brown
- * @Note Simple algorithm that searches for index in a array!
- */
+public class BinarySearchExample implements Runnable {
 
-public class BinarySearch implements Runnable
-{
-    public static void main(String[] args)
-    {
-        Thread thread = new Thread(new BinarySearch());
+	/**
+ 		Author Brandon Jonathan Brown
+	*/
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new BinarySearchExample());
         thread.start();
     }
 
-    public static int BinarySearch(int arr[], int num)
-	  {
+    private static int binarySearch(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length - 1;
 
-        	int low = 0;
-		      int high = arr.length - 1;
+        while (low <= high) {
+            int middlePosition = low + (high - low) / 2; // To prevent potential overflow
+            int middleValue = arr[middlePosition];
 
-		        while(low <= high)
-		        {
-			           int middlePosition = (low + high) / 2;
-			           int middleNumber = arr[middlePosition];
+            if (target == middleValue) {
+                return middlePosition;
+            }
 
-			              if(num == middleNumber)
-				                return middlePosition;
-
-			              if(num < middleNumber)
-				                high = middlePosition - 1;
-			              else
-				                low = middlePosition + 1;
-		        }
-        return -1;
-	}
-
-
-    @Override
-    public void run()
-    {
-
-	int arr[] = new int[] {1, 6, 3, 2, 9, 11, 19, 27};
-	System.out.printf("%S","Enter a number from the array, so you can find the index!");
-	Scanner input = new Scanner(System.in);
-	int n = input.nextInt();
-	System.out.printf("%s%d","Index is ", BinarySearch(arr,n));
-
+            if (target < middleValue) {
+                high = middlePosition - 1;
+            } else {
+                low = middlePosition + 1;
+            }
+        }
+        return -1; // Element not found
     }
 
+    @Override
+    public void run() {
+        int[] arr = {1, 6, 3, 2, 9, 11, 19, 27};
+
+        // Sorting the array before performing binary search
+        Arrays.sort(arr);
+
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+        System.out.print("Enter a number from the array to find its index: ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        scanner.close();
+
+        int index = binarySearch(arr, number);
+
+        if (index != -1) {
+            System.out.println("Index of " + number + " is: " + index);
+        } else {
+            System.out.println(number + " is not in the array.");
+        }
+    }
 }
