5fc3ec7e6645d8ac87437f9bc0e2c0aa6071aa54
Brandon Jonathan Brown <61449380+BrandonJonathanBrown@users.noreply.github.com>
2024-03-29 11:16:52 -0500

    Update EncryptFolder.sh

--- EncryptFolder.sh
+++ EncryptFolder.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 
-echo "Please enter the name for folder to be encrypted:"
-read _name
-if zip -er "$_name".zip *; then
-   echo $? "Done!"
+#Author Brandon Jonathan Brown 
+
+echo "Please enter the name for the folder to be encrypted: "
+read folder_name
+
+if [[ -z "$folder_name" ]]; then
+    echo "Error: No folder name provided. Exiting."
+    exit 1
+fi
+
+if [ ! -d "$folder_name" ]; then
+    echo "Error: The folder '$folder_name' does not exist. Exiting."
+    exit 1
+fi
+
+if ! command -v zip &> /dev/null; then
+    echo "Error: zip command is not available. Please install zip and try again."
+    exit 1
+fi
+
+if zip -er "${folder_name}.zip" "$folder_name"; then
+    # If zip command succeeds, print success message
+    echo "Encryption successful. Created '${folder_name}.zip'."
+else
+    zip_status=$?
+    echo "Error: Failed to encrypt folder '$folder_name'. zip exited with status $zip_status."
+    exit $zip_status
 fi
